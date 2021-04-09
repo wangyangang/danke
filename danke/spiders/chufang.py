25575b024801f6f84d3b39a995fcb53601ccfb10
yangangwang1991 <yangangwang1991@gmail.com>
2021-04-09 12:43:43 +0800

    edit chufang.py

--- danke/spiders/chufang.py
+++ danke/spiders/chufang.py
@@ -13,62 +13,6 @@ import re
 from danke.items import ChufangContractItem, ChufangRenterItem, ChufangUrgencyItem
 
 
-def isLeapYear(years):
-    '''
-    通过判断闰年，获取年份years下一年的总天数
-    :param years: 年份，int
-    :return:days_sum，一年的总天数
-    '''
-    # 断言：年份不为整数时，抛出异常。
-    assert isinstance(years, int), "请输入整数年，如 2018"
-
-    if ((years % 4 == 0 and years % 100 != 0) or (years % 400 == 0)):  # 判断是否是闰年
-        # print(years, "是闰年")
-        days_sum = 366
-        return days_sum
-    else:
-        # print(years, ‘不是闰年‘)
-        days_sum = 365
-        return days_sum
-
-
-def getAllDayPerYear(years):
-    '''
-    获取一年的所有日期
-    :param years:年份
-    :return:全部日期列表
-    '''
-    start_date = '%s-1-1' % years
-    a = 0
-    all_date_list = []
-    days_sum = isLeapYear(int(years))
-    while a < days_sum:
-        b = arrow.get(start_date).shift(days=a).format("YYYY-MM-DD")
-        a += 1
-        all_date_list.append(b)
-    # print(all_date_list)
-    return all_date_list
-
-
-def getAllMonthPerYear(years):
-    '''
-    获取一年的所有日期
-    :param years:年份
-    :return:全部日期列表
-    '''
-    start_date = '%s-1-1' % years
-    end_date = '%s-1-31' % years
-    a = 0
-    all_month_list = []
-    while a < 12:
-        month_start_date = arrow.get(start_date).shift(months=a).format("YYYY-MM-DD")
-        month_end_date = arrow.get(end_date).shift(months=a).format("YYYY-MM-DD")
-        a += 1
-        all_month_list.append((month_start_date, month_end_date))
-    # print(all_date_list)
-    return all_month_list
-
-
 class DkSpider(scrapy.Spider):
     name = 'chufang'
     allowed_domains = ['www.danke.com']
@@ -118,8 +62,8 @@ class DkSpider(scrapy.Spider):
         }
         self.headers.update(cookie=self.cookie)
         page1_ret = requests.get(page1_url, headers=self.headers, params=params)
-        with open('page1.html', 'w') as f:
-            f.write(page1_ret.content.decode())
+        # with open('page1.html', 'w') as f:
+        #     f.write(page1_ret.content.decode())
         # 获取总数据量
         tree = etree.HTML(page1_ret.content.decode())
         # with open('query_data_count.html', 'w') as f:
